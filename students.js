ff916c103860d61f332a143b26ed8b7b30e9a1a6
Turab101 <143023162+Turab101@users.noreply.github.com>
2023-12-12 10:45:38 +0100

    Update students.js

--- students.js
+++ students.js
@@ -34,17 +34,76 @@ export function displayStudents() {
   });
 }
 
-export function addStudent()  {
-  const name = prompt('Name of the Student:');
-  const grade = prompt('Mark of the Student:');
-  if (name && grade) {
-    const newStudent = new Student(Date.now(), name, parseFloat(grade));
-    const students = getStudents();
-    students.push(newStudent);
-    localStorage.setItem('students', JSON.stringify(students));
-    displayStudents();
+export function addStudent() {
+  const existingModal = document.getElementById('addStudentModal');
+  if (existingModal) {
+    existingModal.remove();
   }
+
+  const modalElement = document.createElement('div');
+  modalElement.id = 'addStudentModal';
+  modalElement.className = 'modal fade';
+  modalElement.tabIndex = '-1';
+  modalElement.setAttribute('role', 'dialog');
+  modalElement.setAttribute('aria-labelledby', 'addStudentModalLabel');
+  modalElement.setAttribute('aria-hidden', 'true');
+  modalElement.innerHTML = `
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="addStudentModalLabel">Add New Student</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form id="addStudentForm">
+            <div class="form-group">
+              <label for="studentName">Name</label>
+              <input type="text" class="form-control" id="studentName" required>
+            </div>
+            <div class="form-group">
+              <label for="studentGrade">Grade</label>
+              <input type="text" class="form-control" id="studentGrade" required>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Save Student</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>`;
+
+  document.body.appendChild(modalElement);
+
+  const modal = new bootstrap.Modal(modalElement);
+  modal.show();
+
+  const form = document.getElementById('addStudentForm');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('studentName').value;
+    const grade = document.getElementById('studentGrade').value;
+
+    if (name && grade) {
+      const newStudent = new Student(Date.now(), name, parseFloat(grade));
+      const students = getStudents();
+      students.push(newStudent);
+      localStorage.setItem('students', JSON.stringify(students));
+      displayStudents();
+      modal.hide();
+    }
+  });
+
+  modalElement.addEventListener('hidden.bs.modal', function () {
+    document.body.removeChild(modalElement);
+  });
+  updateCounts()
+
 }
+
+
 
 export function editStudent(id)  {
   let students = getStudents();
